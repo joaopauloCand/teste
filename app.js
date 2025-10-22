437d5f3c8a27675b432baf5d668c8e9bb9841060
Joao Paulo Candido Ribeiro <joaopaulocandidorb@gmail.com>
2025-10-22 19:56:03 -0300

    Simulando conflitos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 99;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+let tentativas = 10;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
